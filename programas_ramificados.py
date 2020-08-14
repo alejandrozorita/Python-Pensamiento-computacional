5ed0c1e169d157001de3357126bb1f2002464619
Alejandro Zorita Hernández <alzort@gmail.com>
2020-08-14 17:51:52 +0200

    enstrucutras if elif

---
+++ programas_ramificados.py
@@ -0,0 +1,9 @@
+num_1 = int(input('Introduce un entero: '))
+num_2 = int(input('Introduce otro entero: '))
+
+if num_1 > num_2:
+    print('El primer número es mayor que el segundo')
+elif num_1 < num_2:
+    print ('el segundo es mayor')
+else:
+    print('los dos num son iguales')
