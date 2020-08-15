d34783fcd3d1fd90969efc811130d834af9268fd
Alejandro Zorita Hernández <alzort@gmail.com>
2020-08-15 18:34:55 +0200

    Create factoriales.py

---
+++ factoriales.py
@@ -0,0 +1,16 @@
+
+def factorial(n):
+    """
+    Calcula el factorial de N
+    :param n: entero > 0
+    :return: n!
+    """
+    print (n)
+    if n == 1:
+        return 1
+
+    return n * factorial(n - 1)
+
+n = int(input('Escribe un entero: '))
+
+print(factorial(n))
