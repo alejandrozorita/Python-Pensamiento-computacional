eb3cca73577a53320f344c58f23a7b0e97a67120
Alejandro Zorita Hernández <alzort@gmail.com>
2020-08-15 13:51:45 +0200

    Create busqueda_binaria.py

---
+++ busqueda_binaria.py
@@ -0,0 +1,18 @@
+objetivo = int(input('Escoge un número: '))
+epsilon = 0.001
+bajo = 0.0
+alto = max(1.0, objetivo)
+respuesta = (alto + bajo) / 2
+
+
+while abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'bajo={bajo}, alto={alto}, respuesa={respuesta}')
+    if respuesta**2 < objetivo:
+        bajo = respuesta
+    else:
+        alto = respuesta
+
+    respuesta = (alto + bajo) / 2
+
+
+print (f'la raiz cuadrada de {objetivo} es {respuesta}')
